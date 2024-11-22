a63673a9e81589bc4bfd5bac7da2fff4f1e87c1f
ajaykr2811 <38921097+ajaykr2811@users.noreply.github.com>
2024-11-23 03:29:04 +0530

    final commit

--- pytest.py
+++ pytest.py
@@ -1,6 +1,21 @@
 import requests
 BASE = "http://127.0.0.1:5000/"
 
-responce = requests.get(BASE + "7").json()
+# responce = requests.get(BASE + "7").json()
 
-print(f'My name is {responce["name"]} and my age is {responce["age"]} and I am from {responce["location"]}')
+# print(f'My name is {responce["name"]} and my age is {responce["age"]} and I am from {responce["location"]}')
+
+payload = {
+    "count": 44,
+    "page": 35,
+    "other": "Something Great"
+}
+i = 1
+while True:
+    res = requests.get("https://httpbin.org/get", params=payload)
+    if res.status_code == 200:
+        break
+    print(f"retrying {i}")
+    i+=1
+
+print(res.text)
